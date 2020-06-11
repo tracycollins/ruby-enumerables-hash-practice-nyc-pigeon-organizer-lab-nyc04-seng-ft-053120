f45c05c8f95cc964133e0e6eb1ec89c53f3cd8a8
Tracy Collins <tc@threeceemedia.com>
2020-06-11 10:14:45 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |entry, value|
+    value.each do |key, pigeonNamesArray|
+      pigeonNamesArray.each do |pigeon_name|
+        pigeon_list[pigeon_name] = {} if pigeon_list[pigeon_name] == nil
+        pigeon_list[pigeon_name][entry] = [] if pigeon_list[pigeon_name][entry] == nil
+        pigeon_list[pigeon_name][entry].push(key.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
